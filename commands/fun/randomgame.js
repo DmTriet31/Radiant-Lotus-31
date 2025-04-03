ed95b26a86c12a26ce8eeea078c60d84bfe78e90
DmTriet31 <dmtriet.31@gmail.com>
2025-04-03 14:21:11 +0200

    Create randomgame.js

---
+++ commands/fun/randomgame.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+// Mảng các trò chơi
+const games = [
+    "Valorant 🎮",
+    "Minecraft ⛏️",
+    "Liên Quân Mobile 📱",
+    "League of Legends 🏆",
+    "Free Fire 🔫"
+];
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('randomgame')
+        .setDescription('Random game cho bạn chơi!'),
+    async execute(interaction) {
+        const randomGame = games[Math.floor(Math.random() * games.length)];
+        await interaction.reply(`Hôm nay, bạn có thể thử chơi: **${randomGame}**`);
+    },
+};
