c24fe7a1af84c91b7944fab4057ee86e15f7941e
DmTriet31 <dmtriet.31@gmail.com>
2025-04-04 01:51:45 +0200

    Update say.js

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -3,20 +3,25 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('say')
-        .setDescription('Bot gửi một tin nhắn embed')
+        .setDescription('Gửi một tin nhắn embed với tiêu đề và nội dung tùy chỉnh')
+        .addStringOption(option => 
+            option.setName('title')
+                .setDescription('Tiêu đề của embed')
+                .setRequired(true))
         .addStringOption(option => 
             option.setName('message')
                 .setDescription('Nội dung bạn muốn bot nói')
                 .setRequired(true)),
-                
+
     async execute(interaction) {
+        const title = interaction.options.getString('title');
         const message = interaction.options.getString('message');
 
         const embed = new EmbedBuilder()
             .setColor(0x00AE86)
-            .setTitle('Thông điệp từ bot:')
+            .setTitle(title)
             .setDescription(message)
-            .setFooter({ text: `Được gửi bởi ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
+            .setFooter({ text: `Gửi bởi ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
             .setTimestamp();
 
         await interaction.reply({ embeds: [embed] });
