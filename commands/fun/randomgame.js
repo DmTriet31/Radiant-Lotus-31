240561905dadc7a12229f3d3f7f31cc1127ef42a
DmTriet31 <dmtriet.31@gmail.com>
2025-04-03 14:44:57 +0200

    Update randomgame.js

--- commands/fun/randomgame.js
+++ commands/fun/randomgame.js
@@ -17,12 +17,18 @@ module.exports = {
         // Random game từ danh sách
         const randomGame = games[Math.floor(Math.random() * games.length)];
 
-        // Tạo embed
+        // Tạo embed với thêm thông tin bổ sung
         const embed = new EmbedBuilder()
             .setColor('#0099ff')
             .setTitle('Random Game')
             .setDescription(`Hôm nay, bạn có thể thử chơi: **${randomGame}**`)
-            .setTimestamp();
+            .addFields(
+                { name: 'Lời khuyên', value: 'Chơi game để giải trí và thư giãn nhé!' }
+            )
+            // Nếu bạn có một ảnh thumbnail, có thể chèn vào đây
+            //.setThumbnail('https://example.com/game-thumbnail.png')
+            .setTimestamp()
+            .setFooter({ text: 'Game Bot', iconURL: 'https://example.com/game-icon.png' });
 
         await interaction.reply({ embeds: [embed] });
     },
