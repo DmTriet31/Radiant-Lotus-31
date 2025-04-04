77e486a9ae86f1692962d50b602750921080ca3c
DmTriet31 <dmtriet.31@gmail.com>
2025-04-04 12:14:31 +0200

    Update say.js

--- commands/moderation/say.js
+++ commands/moderation/say.js
@@ -1,23 +1,27 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('say')
-        .setDescription('Gửi một tin nhắn embed với chữ to')
-        .addStringOption(option => 
-            option.setName('message')
-                .setDescription('Nội dung chính (sẽ hiển thị to hơn)')
-                .setRequired(true)),
+  data: new SlashCommandBuilder()
+    .setName('say')
+    .setDescription('Gửi một embed')
+    .addStringOption(option =>
+      option.setName('title')
+        .setDescription('Tiêu đề embed')
+        .setRequired(true)
+    ),
 
-    async execute(interaction) {
-        const message = interaction.options.getString('message');
+  async execute(interaction) {
+    const title = interaction.options.getString('title');
 
-        const embed = new EmbedBuilder()
-            .setColor(0x00AE86)
-            .setTitle(message) // Tiêu đề sẽ hiển thị to
-            .setFooter({ text: `Gửi bởi ${interaction.user.username}`, iconURL: interaction.user.displayAvatarURL() })
-            .setTimestamp();
+    const embed = new EmbedBuilder()
+      .setColor('#6A0DAD') // Tím đậm
+      .setTitle(title)
+      .setFooter({ 
+        text: `Gửi bởi ${interaction.user.username}`, 
+        iconURL: interaction.user.displayAvatarURL() 
+      })
+      .setTimestamp();
 
-        await interaction.reply({ embeds: [embed] });
-    },
+    await interaction.reply({ embeds: [embed] });
+  },
 };
